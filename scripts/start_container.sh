4a8cffff298bc0129518223c4b7d59f0d7613a51
Mustafa Tavawala <mustafa.t786@gmail.com>
2024-11-15 16:53:56 -0800

    code deploy files

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-docker pull mustafat/simple-python-app:latest
+echo
 
 # Run the Docker image as a container
-docker run -d -p 5000:5000 mustafat/simple-python-app
+echo
